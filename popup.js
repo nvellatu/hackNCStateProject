e5302676655c632c546c73a063e0c04b6e096edf
nvellatu <85042347+nvellatu@users.noreply.github.com>
2026-02-14 12:40:45 -0500

    basic

---
+++ popup.js
@@ -0,0 +1,4 @@
+document.getElementById("greet").addEventListener("click", () => {
+  alert("Hi! Your extension is working! 🚀");
+  // You can also use console.log() — open popup → right-click → Inspect
+});
