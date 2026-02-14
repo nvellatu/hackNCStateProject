1b2c4fa403dc1022c8d95c91f8c3a39ae86f56b4
nvellatu <85042347+nvellatu@users.noreply.github.com>
2026-02-14 12:53:23 -0500

    update structure

--- popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("greet").addEventListener("click", () => {
-  alert("Hi! Your extension is working! 🚀");
-  // You can also use console.log() — open popup → right-click → Inspect
-});
